c77f61f50d4a8215b6986d39e34f4a625b7f7821
Aishwarya6070 <90592693+Aishwarya6070@users.noreply.github.com>
2022-02-18 09:50:27 +0530

    developing we services using JAX for java class

---
+++ PeopleInfo/src/main/java/com/details/info/Details.java
@@ -0,0 +1,7 @@
+package com.details.info;
+
+public interface Details {
+	public void setName(String name);
+	public String getName();
+	
+}
---
+++ PeopleInfo/src/main/java/com/details/info/DetailsInfo.java
@@ -0,0 +1,13 @@
+/**
+ * DetailsInfo.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.details.info;
+
+public interface DetailsInfo extends java.rmi.Remote {
+    public java.lang.String getName() throws java.rmi.RemoteException;
+    public void setName(java.lang.String name) throws java.rmi.RemoteException;
+}
